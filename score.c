8e0dec3a8cebfb55fdc60aa65b20ae8bf4100663
wuzixuan <wuzixuansr@outbook.com>
2017-06-20 17:14:01 +0800

    2017 6 20 17:14
    1.  增加了录入成绩的snore.c文件。但是在新建snore表时出错，提示表不存在。
        如果提前在数据库中建好snore表，则可以通过snore.cgi进行录入成绩功能。
    2.  在index.html中增加了录入成绩功能，增加了增加课程选项（暂未实现）
    3.  将上一版本中部分显示为拼音的地方改成了汉字。

---
+++ score.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+#include "cgic.h"
+
+int cgiMain()
+{
+
+	fprintf(cgiOut, "Content-type:text/html;charset=utf-8\n\n");
+
+
+
+	char stuId[32] = "\0";
+	char score[4] = "\0";
+	char cno[12] = "\0";
+
+	int status = 0;
+
+
+	status = cgiFormString("stuId",  stuId, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get stuId error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("score",  score, 4);
+  if (status != cgiFormSuccess)
+  {
+ 	 fprintf(cgiOut, "get score error!\n");
+ 	 return 1;
+  }
+
+	status = cgiFormString("cno",  cno, 12);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get cno error!\n");
+		return 1;
+	}
+
+
+
+	//fprintf(cgiOut, "name = %s, age = %s, stuId = %s\n", name, age, stuId);
+
+	int ret;
+	char sql[128] = "\0";
+	MYSQL *db;
+
+	//初始化
+	db = mysql_init(NULL);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_init fail:%s\n", mysql_error(db));
+		return -1;
+	}
+
+	//连接数据库
+	db = mysql_real_connect(db, "127.0.0.1", "root", "123456", "Stu",  3306, NULL, 0);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_real_connect fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+
+
+	strcpy(sql,"CREATE TABLE score(stuID INT NOT NULL PRIMARY KEY,score INT NULL,cno INT NOT NULL PRIMARY KEY");
+	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
+	{
+		if (ret != 1)
+		{
+			fprintf(cgiOut,"mysql_real_query fail:%s\n", mysql_error(db));
+			mysql_close(db);
+			return ;
+		}
+	}
+
+
+
+	sprintf(sql,"insert into score values(%d,%d,%d)",atoi(stuId),atoi(score),atoi(cno));
+	if (mysql_real_query(db, sql, strlen(sql) + 1) != 0)
+	{
+		fprintf(cgiOut, "%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+	fprintf(cgiOut, "add score ok!\n");
+	mysql_close(db);
+	return 0;
+}
